439da699533531ae56e76cd4e41184f8a0fb3cdc
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-09 14:47:01 +0700

    add cors package

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config/app')
 const router = require('./router')
 
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const port = config.appPort
 
@@ -11,6 +12,7 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use(router)
 
