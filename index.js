43d97a8e7aa2fe2819b055adf5f03fc88a605ba8
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-11 18:17:55 +0700

    fix config web socket

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+const http = require('http')
 const config = require('./config/app')
 const router = require('./router')
-const cors = require('cors')
+const SocketServer = require('./socket')
+
 const app = express()
-const http = require('http')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,9 +18,9 @@ app.use(router)
 const port = config.appPort
 
 const server = http.createServer(app)
-const SocketServer = require('./socket')
+server.listen(port)
 SocketServer(server)
 
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`server listening on port ${port}`)
+// })
