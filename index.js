ae0e24c96c1b084857a1eb8166b82b479b563848
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-11 17:28:36 +0700

    config socket

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-
 const config = require('./config/app')
 const router = require('./router')
-
 const cors = require('cors')
-
 const app = express()
+const http = require('http')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +14,10 @@ app.use(express.static(__dirname + '/uploads'))
 app.use(router)
 
 const port = config.appPort
+
+const server = http.createServer(app)
+const SocketServer = require('./socket')
+SocketServer(server)
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
