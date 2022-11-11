40dc6bb7caee0dfcc609157ae86ad13a29e1fe0b
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-11 18:43:51 +0700

    query to get chatters in socket

--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,5 @@
 const socketIo = require('socket.io')
-
+const { sequelize } = require('../models')
 const users = new Map()
 
 const SocketServer = (server) => {
@@ -13,13 +13,13 @@ const SocketServer = (server) => {
 
       console.log('New User Joined: ', user.firstName)
 
-      if(users.has(user.id)) {
+      if (users.has(user.id)) {
         const existingUser = users.get(user.id)
         existingUser.sockets = [...existingUser.sockets, ...[socket.id]]
         users.set(user.id, existingUser)
         sockets = [...existingUser.sockets, ...[socket.id]]
       } else {
-        users.set(user.id, {id: user.id, sockets: []})
+        users.set(user.id, { id: user.id, sockets: [] })
         sockets.push(socket.id)
       }
 
@@ -29,12 +29,12 @@ const SocketServer = (server) => {
 
       // notify his friends that user is now online
       for (let i = 0; i < chatters.length; i++) {
-        if(users.has(chatters[i].id)) {
+        if (users.has(chatters[i].id)) {
           const chatter = users.get(chatters[i])
           chatters.sockets.forEach(socket => {
             try {
               io.to(socket).emit('online', user)
-            } catch (error) {}
+            } catch (error) { }
           })
           onlineFriends.push(chatter.id)
         }
@@ -44,12 +44,33 @@ const SocketServer = (server) => {
       sockets.forEach(socket => {
         try {
           io.to(socket).emit('friends', onlineFriends)
-        } catch (error) {}
+        } catch (error) { }
       })
 
       io.to(socket.id).emit('typing', 'User typing...')
     })
   })
+}
+
+const getChatters = async (userId) => {
+  try {
+    const [results, metada] = await sequelize.query(`
+    select "cu"."userId" from "ChatUser" as cu
+    inner join (
+      select "c"."id" from "Chats" as c
+      where exists (
+        select "u"."id" from "Users" as u
+        inner join "ChatUsers" on u.id = "ChatUsers"."userId"
+        where u.id = ${parseInt(userId)} and c.id = "ChatUsers"."chatId"
+      )
+    ) as cjoin on cjoin.id = "cu"."chatId"
+    where "cu"."userId" != ${parseInt(userId)}`)
+
+    return results.length > 0 ? results.map(el => el.userId) : []
+  } catch (error) {
+    console.log(error)
+    return []
+  }
 }
 
 module.exports = SocketServer
