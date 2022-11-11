dd082079517bbe185e8d0f198b25acf742e7a44e
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-11 18:34:21 +0700

    add socket on join event

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,6 @@
-const socketIo = require('socket.io');
+const socketIo = require('socket.io')
+
+const users = new Map()
 
 const SocketServer = (server) => {
   const io = socketIo(server)
@@ -6,7 +8,46 @@ const SocketServer = (server) => {
   io.on('connection', (socket) => {
 
     socket.on('join', async (user) => {
+
+      let sockets = []
+
       console.log('New User Joined: ', user.firstName)
+
+      if(users.has(user.id)) {
+        const existingUser = users.get(user.id)
+        existingUser.sockets = [...existingUser.sockets, ...[socket.id]]
+        users.set(user.id, existingUser)
+        sockets = [...existingUser.sockets, ...[socket.id]]
+      } else {
+        users.set(user.id, {id: user.id, sockets: []})
+        sockets.push(socket.id)
+      }
+
+      const onlineFriends = [] //ids
+
+      const chatters = [] //query 
+
+      // notify his friends that user is now online
+      for (let i = 0; i < chatters.length; i++) {
+        if(users.has(chatters[i].id)) {
+          const chatter = users.get(chatters[i])
+          chatters.sockets.forEach(socket => {
+            try {
+              io.to(socket).emit('online', user)
+            } catch (error) {}
+          })
+          onlineFriends.push(chatter.id)
+        }
+      }
+
+      // send to user sockets which of his friends are online
+      sockets.forEach(socket => {
+        try {
+          io.to(socket).emit('friends', onlineFriends)
+        } catch (error) {}
+      })
+
+      io.to(socket.id).emit('typing', 'User typing...')
     })
   })
 }
