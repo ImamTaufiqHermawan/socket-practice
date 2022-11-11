89f00a6a48d46135e167ba3180685bc7e64f40af
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-11 10:28:52 +0700

    delete chat

--- router/chat.js
+++ router/chat.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
-const { index, create, messages } = require('../controller/chatController')
+const { index, create, messages, deleteChat } = require('../controller/chatController')
 const { validate } = require('../validators')
 const { auth } = require('../middleware/auth')
 
 router.get('/', [auth], index)
 router.get('/messages', [auth], messages)
 router.post('/chat', [auth], create)
+router.delete('/:id', [auth], deleteChat)
 
 module.exports = router
