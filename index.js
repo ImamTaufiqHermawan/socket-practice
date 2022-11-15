b29853744fd1132f053efa18a989e52be7ddb8a7
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-15 20:41:38 +0700

    fix socket server

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const express = require('express')
-const cors = require('cors')
-const http = require('http')
 const config = require('./config/app')
 const router = require('./router')
-const SocketServer = require('./socket')
-
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
+const http = require('http')
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(cors())
+app.use(router)
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/uploads'))
-
-app.use(router)
 
 const port = config.appPort
 
 const server = http.createServer(app)
-server.listen(port)
+const SocketServer = require('./socket')
 SocketServer(server)
 
-// app.listen(port, () => {
-//   console.log(`server listening on port ${port}`)
-// })
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
