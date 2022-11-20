6269cf153277d3c5f9409f19df25f11c202f7ab5
imam.hermawan <imam.hermawan@gramedia.id>
2022-11-20 23:34:18 +0700

    add new friend to group chat

--- router/chat.js
+++ router/chat.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
-const { index, create, messages, deleteChat, imageUpload } = require('../controller/chatController')
+const { index, create, messages, deleteChat, imageUpload, addUserToGroup } = require('../controller/chatController')
 const { validate } = require('../validators')
 const { auth } = require('../middleware/auth')
-const {chatFile} = require('../middleware/fileUpload')
+const { chatFile } = require('../middleware/fileUpload')
 
 router.get('/', [auth], index)
 router.get('/messages', [auth], messages)
 router.post('/create', [auth], create)
 router.post('/upload-image', [auth, chatFile], imageUpload)
+router.post('/add-user-to-group', auth, addUserToGroup)
 router.delete('/:id', [auth], deleteChat)
 
 module.exports = router
